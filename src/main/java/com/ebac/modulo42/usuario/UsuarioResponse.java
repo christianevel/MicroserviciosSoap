a343aae8723a324e27d3eb075b1e5c95ad3fa3b9
christianevel <christianevel4@gmail.com>
2024-04-06 18:37:31 -0600

    Proyecto basico con soporte a Spring Boot para la creacion de un servicio web Soap

---
+++ src/main/java/com/ebac/modulo42/usuario/UsuarioResponse.java
@@ -0,0 +1,71 @@
+//
+// Este archivo ha sido generado por Eclipse Implementation of JAXB v3.0.0 
+// Visite https://eclipse-ee4j.github.io/jaxb-ri 
+// Todas las modificaciones realizadas en este archivo se perderán si se vuelve a compilar el esquema de origen. 
+// Generado el: 2024.04.06 a las 01:26:30 AM CST 
+//
+
+
+package com.ebac.modulo42.usuario;
+
+import jakarta.xml.bind.annotation.XmlAccessType;
+import jakarta.xml.bind.annotation.XmlAccessorType;
+import jakarta.xml.bind.annotation.XmlElement;
+import jakarta.xml.bind.annotation.XmlRootElement;
+import jakarta.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Clase Java para anonymous complex type.
+ * 
+ * <p>El siguiente fragmento de esquema especifica el contenido que se espera que haya en esta clase.
+ * 
+ * <pre>
+ * &lt;complexType&gt;
+ *   &lt;complexContent&gt;
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType"&gt;
+ *       &lt;sequence&gt;
+ *         &lt;element name="saludo" type="{http://www.w3.org/2001/XMLSchema}string"/&gt;
+ *       &lt;/sequence&gt;
+ *     &lt;/restriction&gt;
+ *   &lt;/complexContent&gt;
+ * &lt;/complexType&gt;
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "", propOrder = {
+    "saludo"
+})
+@XmlRootElement(name = "UsuarioResponse")
+public class UsuarioResponse {
+
+    @XmlElement(required = true)
+    protected String saludo;
+
+    /**
+     * Obtiene el valor de la propiedad saludo.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getSaludo() {
+        return saludo;
+    }
+
+    /**
+     * Define el valor de la propiedad saludo.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setSaludo(String value) {
+        this.saludo = value;
+    }
+
+}
---
+++ src/main/java/com/ebac/modulo42/usuario/package-info.java
@@ -0,0 +1,9 @@
+//
+// Este archivo ha sido generado por Eclipse Implementation of JAXB v3.0.0 
+// Visite https://eclipse-ee4j.github.io/jaxb-ri 
+// Todas las modificaciones realizadas en este archivo se perderán si se vuelve a compilar el esquema de origen. 
+// Generado el: 2024.04.06 a las 01:26:30 AM CST 
+//
+
+@jakarta.xml.bind.annotation.XmlSchema(namespace = "http://modulo42.ebac.com/usuario", elementFormDefault = jakarta.xml.bind.annotation.XmlNsForm.QUALIFIED)
+package com.ebac.modulo42.usuario;
